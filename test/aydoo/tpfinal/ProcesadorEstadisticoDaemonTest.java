2eab1a9cfc93e9bb5eb95c46949472fbad5de7cc
martinffg <martinffg@gmail.com>
2015-06-27 15:54:59 -0300

    Commit que soluciona el loop infinito usado para test del Daemon en forma local

--- test/aydoo/tpfinal/ProcesadorEstadisticoDaemonTest.java
+++ test/aydoo/tpfinal/ProcesadorEstadisticoDaemonTest.java
@@ -5,6 +5,9 @@ import org.junit.Assert;
 
 public class ProcesadorEstadisticoDaemonTest  {
 
+	/*
+	// Se comenta este test porque genera un loop dejando el servicioa la espera de ingreso de archivos en un folder
+	// de esta forma, cuelga Travis y hace fallar Jenkins.	
 	@Test
 	public void testEjecucionDaemon() {
 		String pathDeEntrada="entradas_tests/";
@@ -14,7 +17,7 @@ public class ProcesadorEstadisticoDaemonTest  {
 		
 		Assert.assertNotNull(procesadorEstadistico);
 	
-	}
+	}*/
 	
 	@Test
 	public void testGenerarInformeDaemon() {
