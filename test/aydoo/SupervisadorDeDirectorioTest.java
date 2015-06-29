62052881b77933c6408a7c5d6d1341b072d1c3a6
martinffg <martinffg@gmail.com>
2015-06-29 03:06:25 -0300

    Commit SupervisadorDeDirectorioTest faltante

--- test/aydoo/SupervisadorDeDirectorioTest.java
+++ test/aydoo/SupervisadorDeDirectorioTest.java
@@ -1,14 +1,27 @@
 package aydoo;
 
+import org.junit.Assert;
 //import java.util.concurrent.TimeUnit;
-//import org.junit.Assert;
 import org.junit.Test;
 
 
 public class SupervisadorDeDirectorioTest {
 	
 	@Test
-	public void test() {
+	public void pruebaDeCreacion(){
+		
+		String directorio="entradas_tests/";
+			
+		ProcesadorEstadisticoDaemon procesadorDaemon=new ProcesadorEstadisticoDaemon(directorio);
+
+		SupervisadorDeDirectorio supervisor = new SupervisadorDeDirectorio(directorio,procesadorDaemon);
+		
+		Assert.assertNotNull(supervisor);
+		
+	}
+	
+	@Test
+	public void testDeEjecucion() {
 		
 		// ESTE TEST NO LO CORREMOS PORQUE GENERA UN LOOP INFINITO PARA EL SERVICIO QUE MONITOREA EL PATH DE ENTRADA
 		// SEGUN LO ACORDADO, SE DEJA COMENTADO ESTE CODIGO, AUNQUE ESTE TEST ESTA CORRIENDO SIN PROBLEMAS.
